3b3ce71e7970cfba1d77663dba3dccc17a3939e4
Sail <m17600576201@163.com>
2021-10-11 18:10:38 +0800

    完善登陆方法

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8082'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
