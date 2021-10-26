e08a26ff95d65a73bde2855228b5de6ac39a503c
Sail <m17600576201@163.com>
2021-10-26 15:03:28 +0800

    新增用户

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -52,7 +52,8 @@
     <el-dialog
       title="添加用户"
       :visible.sync="userVisible"
-      width="50%">
+      width="50%"
+      @close="addDialogClosed">
       <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
         <el-form-item label="用户名" prop="username">
           <el-input v-model="addForm.username" placeholder="请输入用户名"></el-input>
@@ -72,7 +73,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button @click="userVisible = false">取 消</el-button>
-    <el-button type="primary" @click="userVisible = false">确 定</el-button>
+    <el-button type="primary" @click="addUser">确 定</el-button>
   </span>
     </el-dialog>
   </div>
@@ -96,7 +97,7 @@ export default {
       }
       cb(new Error('请输入合法手机号'))
     }
-    console.log(checkPhone, checkEmail)
+
     return {
       queryInfo: {
         query: '',
@@ -163,6 +164,21 @@ export default {
         return this.$message.error('更新用户状态失败')
       }
       return this.$message.success('更新用户状态成功')
+    },
+    addDialogClosed () {
+      this.$refs.addFormRef.resetFields()
+    },
+    addUser () {
+      this.$refs.addFormRef.validate(async vaild => {
+        if (!vaild) return
+        const { data: res } = await this.$http.post('/auth/addUser', this.addForm)
+        if (res.code !== 20000) {
+          return this.$message.error('添加用户失败')
+        }
+        this.userVisible = false
+        this.getUserList()
+        return this.$message.success('添加用户成功')
+      })
     }
   }
 }
