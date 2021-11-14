311e0e7061f021e3bed8ca9140ede16243f52329
liuwenfan <afan6203@126.com>
2021-11-15 00:08:58 +0800

    角色权限列表

--- src/components/permissions/Permission.vue
+++ src/components/permissions/Permission.vue
@@ -6,14 +6,34 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      123
+      <el-table :data="permissions" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="权限名称" prop="permissionsList"></el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      permissions: []
+    }
+  },
+  created () {
+    this.permissionslist()
+  },
+  methods: {
+    async permissionslist () {
+      const { data: res } = await this.$http.get('/role/getRoleList')
+      if (res.code !== 20000) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.permissions = res.data
+    }
+  }
 }
 </script>
 
