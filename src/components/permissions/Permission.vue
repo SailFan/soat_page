34accbc2b3c8933f302fd8603f108b65f62d70c0
liuwenfan <afan6203@126.com>
2021-11-25 00:01:43 +0800

    删除权限

--- src/components/permissions/Permission.vue
+++ src/components/permissions/Permission.vue
@@ -12,13 +12,19 @@
       <el-table :data="permissions" border stripe>
         <el-table-column type="expand">
           <template slot-scope="scope">
-            <span v-if="scope.row.permissionsList.length==0">暂无权限</span>
-            <el-row v-else :class="['bdbottom', i1 === 0 ? 'bdtop' : '']" v-for="(item1, i1) in scope.row.permissionsList" :key="item1.pid">
-              <el-col :span="10">
-                <el-tag>{{item1.name}}</el-tag>
-                <i class="el-icon-caret-right"></i>
+            <el-tag type="info" v-if="scope.row.permissionsList.length==0">暂无权限</el-tag>
+<!--            <el-row style="display: flex;align-items: center" v-else :class="['bdbottom', i1 === 0 ? 'bdtop' : '']"  v-for="(item1, i1) in scope.row.permissionsList" :key="item1.pid">-->
+            <el-row v-else>
+              <el-col :span="24">
+                <el-tag closable  type="success" v-for="(item1) in scope.row.permissionsList" :key="item1.pid" @close="removePermission(scope.row,item1.pid)">{{item1.name}}</el-tag>
               </el-col>
-              <el-col :span="14"></el-col>
+<!--              <el-col :span="14">-->
+<!--                <el-row>-->
+<!--                  <el-col>-->
+<!--                    <el-tag closable @close="removePermission(scope.row,item2.pcid)" v-for="(item2) in item1.queryChildrePermission" :key="item2.pcId" type="success">{{item2.name}}</el-tag>-->
+<!--                  </el-col>-->
+<!--                </el-row>-->
+<!--              </el-col>-->
             </el-row>
           </template>
         </el-table-column>
@@ -54,6 +60,22 @@ export default {
         return this.$message.error('获取角色列表失败')
       }
       this.permissions = res.data
+    },
+    async removePermission (role, pid) {
+      const result = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (result !== 'confirm') {
+        return this.$message.info('用户取消删除')
+      }
+      const { data: res } = await this.$http.get('/role/removeRolePermission', { params: role.rid, pid })
+      if (res.code !== 20000) {
+        return this.$message.error('删除权限失败')
+      }
+      this.permissionslist()
+      this.$message.success('权限删除成功')
     }
   }
 }
