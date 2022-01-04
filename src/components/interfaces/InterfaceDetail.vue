b3959ca550a1257037c259f4c2125551e0107e54
Sail <m17600576201@163.com>
2022-01-04 19:03:23 +0800

    fixed

--- src/components/interfaces/InterfaceDetail.vue
+++ src/components/interfaces/InterfaceDetail.vue
@@ -9,7 +9,59 @@
       <template>
         <el-tabs v-model="activeName" @tab-click="handleClick" type="border-card">
           <el-tab-pane label="预览" name="first">预览111</el-tab-pane>
-          <el-tab-pane label="编辑" name="second">配置管理</el-tab-pane>
+          <el-tab-pane label="编辑" name="second">
+            <h1 style="font-weight: lighter">基本设置</h1>
+            <div class="base">
+            <el-form>
+              <el-form-item label="活动名称" label-width="80px">
+                <el-input></el-input>
+              </el-form-item>
+              <el-form-item label="选择分类" label-width="80px">
+                <el-select placeholder="请选择分类">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+
+              <el-form-item label="接口路径" label-width="80px">
+                <el-input placeholder="请输入内容" class="input-with-select">
+                  <el-select  v-model="select" slot="prepend" placeholder="请选择">
+                    <el-option label="GET" value="1"></el-option>
+                    <el-option label="POST" value="2"></el-option>
+                    <el-option label="PUT" value="3"></el-option>
+                    <el-option label="DELETE" value="4"></el-option>
+                  </el-select>
+                  <el-button slot="append" icon="el-icon-search"></el-button>
+                </el-input>
+              </el-form-item>
+
+            </el-form>
+            </div>
+            <h1 style="font-weight: lighter">请求参数设置</h1>
+            <div class="base">
+              <el-form>
+                <el-form-item label="活动名称">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-form>
+            </div>
+            <h1 style="font-weight: lighter">返回数据设置</h1>
+            <div class="base">
+              <el-form>
+                <el-form-item label="活动名称">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-form>
+            </div>
+            <h1 style="font-weight: lighter">其他</h1>
+            <div class="base">
+              <el-form>
+                <el-form-item label="活动名称">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-form>
+            </div>
+          </el-tab-pane>
           <el-tab-pane label="运行" name="third">角色管理</el-tab-pane>
         </el-tabs>
       </template>
@@ -21,7 +73,8 @@
 export default {
   data () {
     return {
-      activeName: 'first'
+      activeName: 'first',
+      select: ''
     }
   },
   methods: {
@@ -30,4 +83,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.base {
+  background-color: #dddddd;
+  border-radius: 15px;
+  background: rgba(40%,40%,40%,0.09);
+  padding: 15px;
+
+}
 </style>
